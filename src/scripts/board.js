2d9dec5abdb307331c95d54cc501544d07b7f042
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-15 15:26:16 -0500

    added a red background whenever a coin is clicked

--- src/scripts/board.js
+++ src/scripts/board.js
@@ -5,52 +5,41 @@ export class Board {
   }
 
   createBoardArray() {
-    console.log("array")
 
     let arr = [];
     for (let i = 0; i < 8; i++) {
       const row = [];
       for (let j = 0; j < 8; j++) {
         const randomValue = Math.random();
-        const cellValue = randomValue <= 0.2 ? "b" : "y";
+        const cellValue = randomValue <= 0.2 ? "💣" : "💰";
         row.push(cellValue);
-        // console.log(row)
       }
       arr.push(row);
     }
-    // console.log(arr[3][5])
     return arr;
   }
 
   createBoard() {
     for (let r = 0; r < 8; r++) {
       for (let c = 0; c < 8; c++) {
-        let tile = document.createElement("li");
-        // tile.dataset.pos = JSON.stringify([r, c]);
+        let tile = document.createElement("li"); 
         tile.dataset.posX = r;
         tile.dataset.posY = c;
-        // tile.dataset.pos = ([r, c]);
         this.boardContainer.appendChild(tile);
       }
     }
-    // this.createBoardArray()
     this.boardContainer.addEventListener("click", this.handleClick.bind(this));
   }
 
   handleClick(e) {
-    console.log("hello");
-    console.log(e)
-    console.log(e.target)
+    // console.log("hello");
+    // console.log(e)
+    // console.log(e.target)
     const dataPosValue = [parseInt(e.target.dataset.posX), parseInt(e.target.dataset.posY)];
-    // console.log(dataPosValue.prototype);
-    console.log((dataPosValue))
-    // console.log(this.boardArray);
-
-    // if(this.boardArray[])
     
-    // console.log(this.arr)
-    if (this.boardArray[dataPosValue[0]][dataPosValue[1]] === "y"){
-      console.log("hellooooooo")
+    if (this.boardArray[dataPosValue[0]][dataPosValue[1]] === "💰"){
+      e.target.classList.add('clicked')
+
     }
   }
 }
