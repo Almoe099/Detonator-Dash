687ff8e1efe51efa9115a50e896ea544b791261f
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-18 17:55:30 -0500

    added a pop up but still needs styling

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const board = document.getElementById("board")
     const game = new Game(board)
 
+    
     const open = document.getElementById("open");
     const modalContainer = document.getElementById("modalContainer");
     const close = document.getElementById("close");
-
+    
     open.addEventListener("click", () => {
         modalContainer.classList.add("show")
     })
     close.addEventListener("click", () => {
         modalContainer.classList.remove("show")
     })
-
-
+    
 });
--- src/scripts/board.js
+++ src/scripts/board.js
@@ -158,4 +158,5 @@ export class Board {
       this.updateScoreDisplay();
     }, 1000);
   }
+  
 }
